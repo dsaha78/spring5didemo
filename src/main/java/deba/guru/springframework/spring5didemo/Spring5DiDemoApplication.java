cb8c8a9c8ac2e696022da70dc683717f46828ad9
dsaha78 <debashish.saha@gmail.com>
2017-11-28 16:19:26 +0530

    first commit

--- src/main/java/deba/guru/springframework/spring5didemo/Spring5DiDemoApplication.java
+++ src/main/java/deba/guru/springframework/spring5didemo/Spring5DiDemoApplication.java
@@ -4,14 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import deba.guru.springframework.spring5didemo.controller.ConstructorInjectorController;
 import deba.guru.springframework.spring5didemo.controller.MyController;
+import deba.guru.springframework.spring5didemo.controller.PropertyInjectedController;
 
 @SpringBootApplication
 public class Spring5DiDemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Spring5DiDemoApplication.class, args);
-		MyController myController = (MyController)ctx.getBean("myController");
-		myController.hello();
+		//MyController myController = (MyController)ctx.getBean("myController");
+		//myController.hello();
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		
+		//System.out.println(ctx.getBean(ConstructorInjectorController.class).sayHello());
+		
 	}
 }
